a433fe747ba79739ad9978dfb2d674680b080da1
Iman Jalali <i.jalali88@gmail.com>
2024-07-22 12:25:59 +0330

    create TitleIcon component

---
+++ src/components/TitleIcon.jsx
@@ -0,0 +1,18 @@
+import PropTypes from "prop-types";
+import MyIcon from "./MyIcon";
+
+const TitleIcon = ({ iconName, title }) => {
+  return (
+    <div className="inline-flex">
+      <MyIcon iconName={iconName} size="20" />
+      <p className=" ml-1">{title}</p>
+    </div>
+  );
+};
+
+export default TitleIcon;
+
+TitleIcon.propTypes = {
+  iconName: PropTypes.string.isRequired,
+  title: PropTypes.string,
+};
