6b351ae31ebc73d5d45c33b546b82876ba065e51
Iman Jalali <i.jalali88@gmail.com>
2024-07-22 12:24:29 +0330

    create DescriptionMovie component

---
+++ src/components/DescriptionMovie.jsx
@@ -0,0 +1,11 @@
+export default function DescriptionMovie() {
+  return (
+    <div className="font-serif text-sm font-medium rounded-xl border border-black_15 p-6">
+      <h1 className="text-gray_60">Description</h1>
+      <p className="text-white">
+        A fiery young man clashes with an unflinching forest officer in a south
+        Indian village where spirituality, fate and folklore rule the lands.
+      </p>
+    </div>
+  );
+}
