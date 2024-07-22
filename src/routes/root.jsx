8b70e113e8aaeb6af7df9f05fa1fe0c414a2d9a6
Iman Jalali <i.jalali88@gmail.com>
2024-07-22 12:31:11 +0330

    create new rout - /movies/:movieId

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Movies from "./Movies";
+import Movie from "./Movie";
 
 const router = createBrowserRouter([
   {
@@ -14,6 +15,10 @@ const router = createBrowserRouter([
       {
         path: "/movies",
         element: <Movies />,
+      },
+      {
+        path: "/movies/:movieId",
+        element: <Movie />,
       },
     ],
   },
