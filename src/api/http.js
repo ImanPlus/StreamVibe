f6401cc368e139ef0ae83cb54cb9f69a58110359
Iman Jalali <i.jalali88@gmail.com>
2024-08-30 18:41:17 +0330

    feat: Extract base-url from .env file

--- src/api/http.js
+++ src/api/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const createAxiosInstance = () => {
-  const baseURL = process.env.REACT_APP_BASE_URL;
+  const baseURL = import.meta.env.REACT_APP_BASE_URL;
   const instance = axios.create({ baseURL });
   return instance;
 };
